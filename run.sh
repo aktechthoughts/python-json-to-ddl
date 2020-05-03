7740ba8ce670c9a761ec52bb361a9fbcf39b7746
Add to docker group <ubuntu@ip-10-0-2-107.eu-central-1.compute.internal>
2020-05-03 21:07:55 +0000

    Loads data from S3 to mysql

--- run.sh
+++ run.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-#aws s3 ls s3://aktechthoughts/tweet_2020/ | cut -c32- | while read filename
-ls data  | while read filename 
+aws s3 ls s3://aktechthoughts/tweet_2020/ | cut -c32- | while read filename
+#ls data  | while read filename 
 do
 aws s3 cp s3://aktechthoughts/tweet_2020/$filename ./data
 python3 insert_json_data.py data/$filename
-cat data/$filename >> Consolidated_Tweets.json
+cat data/$filename >> data/Consolidated_Tweets.json
 rm data/$filename
 done
 
 
-
-mysql -h "$HOST_NAME" -u "$USER_NAME" -p "$PASSWORD" "$DB_NAME" < "sql/Tweets.Texts.sql"
-mysql -h "$HOST_NAME" -u "$USER_NAME" -p "$PASSWORD" "$DB_NAME" < "sql/Tweets.Users.sql"
-
+mysql --host="$DB_HOST_NAME" --user="$USER_NAME" --password="$PASSWORD" "$DB_NAME" < sql/Tweets.Users.sql 
+mysql --host="$DB_HOST_NAME" --user="$USER_NAME" --password="$PASSWORD" "$DB_NAME" < sql/Tweets.Texts.sql
